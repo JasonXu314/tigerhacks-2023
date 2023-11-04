bdb086f3abcd3c59a671c762de5cb0177453b225
Jason Xu <Xinwen31415@gmail.com>
2023-11-03 19:47:21 -0500

    yeeted frontend

---
+++ backend/prisma/migrations/20231104004155_initial_schema/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE `Room` (
+    `id` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Player` (
+    `id` INTEGER NOT NULL,
+    `roomId` VARCHAR(191) NOT NULL,
+    `score` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Player` ADD CONSTRAINT `Player_roomId_fkey` FOREIGN KEY (`roomId`) REFERENCES `Room`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -3,5 +3,8 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-	public async onModuleInit() {}
+	public async onModuleInit() {
+		await this.$connect();
+	}
 }
+
