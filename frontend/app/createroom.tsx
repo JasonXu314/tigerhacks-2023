354dc3b0c02a8859aa0ec54e725630d17f1e0735
Aiturgan <aiturgan888@gmail.com>
2023-11-03 23:30:43 -0500

    wrote frontend for index, added btns

--- frontend/app/createroom.tsx
+++ frontend/app/createroom.tsx
@@ -1,16 +1,16 @@
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import BackButton from '../components/BackButton';
 import SongSelector from '../components/SongSelector';
 import { useState, useEffect } from 'react';
 
 const CreateRoomScreen = () => {
-    const [songSelectorVisible, setSongSelectorVisible] = useState(false);
+	const [songSelectorVisible, setSongSelectorVisible] = useState(false);
 
 	return (
 		<SafeAreaView style={styles.container}>
-            <BackButton></BackButton>
-            <SongSelector modalVisible={songSelectorVisible} setModalVisible={setSongSelectorVisible}></SongSelector>
+			<BackButton></BackButton>
+			<SongSelector modalVisible={songSelectorVisible} setModalVisible={setSongSelectorVisible}></SongSelector>
 			<Text>Hello</Text>
 		</SafeAreaView>
 	);
--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ImageBackground } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useRouter } from 'expo-router';
 import BackButton from '../components/BackButton';
@@ -19,13 +19,22 @@ const HomeScreen = () => {
 	};
 
 	return (
-		<SafeAreaView style={styles.container}>
-			<BackButton></BackButton>
-			<Text>Hello</Text>
-			<TouchableOpacity onPress={() => createRoom()}>
-				<Text>Create Room</Text>
-			</TouchableOpacity>
-		</SafeAreaView>
+		<ImageBackground
+			source={require('../assets/images/BackgroundPic/HeroPageWMic.png')}
+			imageStyle={{ resizeMode: 'cover' }}
+			style={{ height: '100%', width: '100%' }}
+		>
+			<SafeAreaView style={styles.container}>
+				<BackButton></BackButton>
+				<Text style={styles.p}>Hello</Text>
+				<TouchableOpacity style={styles.btn} onPress={() => createRoom()}>
+					<Text style={styles.btnText}>Create Room</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.btn} onPress={() => createRoom()}>
+					<Text style={styles.btnText}>Join Room</Text>
+				</TouchableOpacity>
+			</SafeAreaView>
+		</ImageBackground>
 	);
 };
 
@@ -35,6 +44,25 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		justifyContent: 'center',
 		padding: 20,
+	},
+	btn: {
+		marginTop: 20,
+		backgroundColor: '#C2E812',
+		borderRadius: 30,
+		paddingVertical: 12,
+		width: 200,
+	},
+	btnText: {
+		fontFamily: 'Neulis500',
+		color: '#210461',
+		fontSize: 20,
+		textAlign: 'center',
+	},
+	p: {
+		fontFamily: 'Neulis500',
+		color: '#210461',
+		fontSize: 20,
+		textAlign: 'center',
 	},
 });
 
